d73865a18146b3162b4f4a1c0c2ca6fc49135b9f
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-16 09:58:37 +0700

    ADD : Make about components

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 
 import Headers from './components/Headers'
+import About from './components/About'
 
 const App = () => {
   return (
     <div className='w-full overflow-hidden'>
    
       <Headers/>
+      <About/>
     </div>
   )
 }

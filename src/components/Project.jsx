8976c9fd2dcf77a7114607c8ab8859d08f9405f6
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-17 17:04:29 +0700

    ADD : Buntton next and previosu in project components

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import {assets} from '../assets/assets'
+
 const Project = () => {
   return (
     <div className='conatiner mx-auto py-4 pt-20 px-6 md:px-20
@@ -7,6 +9,16 @@ const Project = () => {
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects 
         <span className='underlien underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
       <p className='text-center text-gray-500 mb-8 max-w-80 mx-auto'>Crafting Spaces, Exploring Our Portofolio </p>
+    
+    {/* Slide buttons  */}
+    <div className='flex justify-end items-center mb-8'>
+        <button className='p-3 bg-gray-200 rounded-full' aria-label='Previous'>
+            <img src={assets.left_arrow} alt="Previous" />
+        </button>
+        <button className='p-3 bg-gray-200 rounded-full' aria-label='Next'>
+            <img src={assets.right_arrow} alt="Netx" />
+        </button>
+    </div>
     </div>
   )
 }

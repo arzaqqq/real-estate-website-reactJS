6feb4c17b237e376c7ddd9f106d74dac1334f801
Arzaqqq <mmiftahularzaq01@gmail.com>
2025-02-17 17:22:43 +0700

    UPDATE : Crad Data projects

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import {assets} from '../assets/assets'
+import {assets, projectsData} from '../assets/assets'
 
 const Project = () => {
   return (
@@ -12,12 +12,33 @@ const Project = () => {
     
     {/* Slide buttons  */}
     <div className='flex justify-end items-center mb-8'>
-        <button className='p-3 bg-gray-200 rounded-full' aria-label='Previous'>
+        <button className='p-3 bg-gray-200 rounded-full mr-2' aria-label='Previous'>
             <img src={assets.left_arrow} alt="Previous" />
         </button>
-        <button className='p-3 bg-gray-200 rounded-full' aria-label='Next'>
+        <button className='p-3 bg-gray-200 rounded-full mr-2' aria-label='Next'>
             <img src={assets.right_arrow} alt="Netx" />
         </button>
+    </div>
+
+    {/* Projectc slider container */}
+    <div className='overflow-hidden'>
+        <div className="flex gap-8 transition-transform duration-500 ease-in-out">
+            {projectsData.map((project, index) => (
+                <div key={index} className="relative flex-shrink-0 w-full sm:w-1/4">
+                    <img src={project.image} alt={project.title} className='w-full h-auto mb-14'/>
+                <div className="absolute left-0 right-0 bottom-5 flex justify-center">
+                    <div className='inline-block bg-white px-4 py-2 rounded-full shadow-md'>
+                        <h2 className='text-xl font-semibold text-gray-800'>
+                            {project.title}
+                        </h2>
+                        <p className='text-gray-500 text-sm'>
+                            {project.price} <span>|</span> {project.location}
+                        </p>
+                    </div>
+                </div>
+                </div>
+            ))}
+        </div>
     </div>
     </div>
   )
